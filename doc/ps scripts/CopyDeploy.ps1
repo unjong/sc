b8ac1933daa1611690ccafc1576d8b4608387a4f
uj <achunja@gmail.com>
2015-07-30 16:19:10 +0900

    123

---
+++ doc/ps scripts/CopyDeploy.ps1
@@ -0,0 +1,27 @@
+﻿Clear-Host
+
+$FromDir = "\\10.10.11.20\NugetPackages\Deploy"
+$ToDir = "D:\720.YUHS\Useverance3\src\Common\artifacts\Debug"
+$Filter = "*.dll"
+#Error	53	Metadata file 'D:\720.YUHS\Useverance3\src\Common\artifacts\Debug\His3.Hp.Client.Com.dll' could not be found	D:\720.YUHS\Useverance3\src\Sp\Client\Blo\CSC	His3.Sp.Client.Blo
+#Error	54	Metadata file 'D:\720.YUHS\Useverance3\src\Common\artifacts\Debug\His3.Sp.Client.Com.dll' could not be found	D:\720.YUHS\Useverance3\src\Sp\Client\Blo\CSC	His3.Sp.Client.Blo
+
+$sw = [System.Diagnostics.Stopwatch]::startNew()
+$sw.Start()
+
+Get-ChildItem -path $FromDir -recurse -Include $Filter | 
+    where { $_.FullName } | 
+    Foreach-Object { 
+    $_
+    Copy-Item $_ -Destination $ToDir
+    }
+
+#Foreach-Object -Begin { If($_ -eq $null){ continue }} -Process { Copy-Item $_ -Destination $ToDir }
+#11.8747187
+
+#XCOPY "\\10.10.11.20\NugetPackages\Deploy\*.dll" "D:\720.YUHS\Useverance3\src\Common\artifacts\Debug" /S /I /Y 
+#14.4612212
+
+$Sw.Stop()
+$Sw.Elapsed.TotalSeconds
+
---
+++ doc/ps scripts/ExcelOpen.ps1
@@ -0,0 +1,27 @@
+﻿Clear-Host
+
+$Directory = "D:\720.YUHS\SharedData\_DocData\81.전체화면분석\SP"
+$SearchFileFilter = "*EmrUnitDF*"
+
+ #%{ $_.FullName } 
+$Files = Get-ChildItem -Path $Directory -Filter $SearchFileFilter -Recurse | Select-Object -ExpandProperty FullName
+if ($Files.Count -gt 1)
+{
+    $SelectedPath =  $Files | Out-GridView -Title "Search Results" -Pass
+}
+else
+{
+    $SelectedPath= $Files
+}
+
+if ($SelectedPath.Length -lt 1) { exit }
+
+#$objExcel = New-Object -ComObject Excel.Application
+#$WorkBook = $objExcel.Workbooks.Open($SelectedPath)
+
+$xl = New-Object -comobject Excel.Application
+# Show Excel
+$xl.visible = $true
+$xl.DisplayAlerts = $False
+# Create a workbook
+$wb = $xl.Workbooks.open($SelectedPath) 
